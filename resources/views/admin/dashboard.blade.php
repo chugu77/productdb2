dbbefb8e1bedd652c8c572776c4d1fa716a73037
Nick <nicktsiklauri@gmail.com>
2020-05-22 23:25:34 +0400

    dashboard make up with bs4 :))

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,51 @@
 @extends('admin.layouts.app_admin')
 
 @section('content')
-    <h1> admin </h1>
+    <h1> Admin Dashboard </h1>
+    <div class="row">
+        <div class="col">
+            <div class="jumbotron jumbotron-fluid bg-info">
+                <div class="container">
+                    <h1>Categories 10</h1>
+                    <p class="lead">count of ategories</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="jumbotron jumbotron-fluid bg-success">
+                <div class="container">
+                    <h1>Products 22</h1>
+                    <p class="lead">count of products</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="jumbotron jumbotron-fluid bg-warning">
+                <div class="container">
+                    <h1>Users 120</h1>
+                    <p class="lead">registered users count</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col">
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <button type="button" class="btn btn-lg btn-outline-primary btn-block">Add New Category</button>
+                    <p class="lead text-secondary">some info</p>
+                </div>
+        </div>
+        </div>
+        <div class="col">
+          <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <button type="button" class="btn btn-lg btn-outline-success btn-block">Add New Product</button>
+              <p class="lead text-secondary">some info</p>
+            </div>
+          </div>
+        </div>
+    </div>
+
 @endsection

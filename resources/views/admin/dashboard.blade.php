46e1460d5b4737191656cf2d6a7d86e541853d01
Nick <nicktsiklauri@gmail.com>
2020-05-22 21:10:21 +0400

    installed kalnoy/nestedset ; make:auth & admin /routes/controller/views/admin-user-seeder;

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+    <h1> admin </h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'auth'], function() {
+    route::get('/', 'DashboardController@index')->name('admin.index');
+});
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', function (){
+    return redirect(route('admin.index'));
+})->name('home');
